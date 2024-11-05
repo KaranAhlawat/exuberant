069d9f67c5eb3b713f019f0b56fd3323a2541ebc
Karan Ahlawat <ahlawatkaran12@gmail.com>
2024-11-05 21:14:37 +0530

    decent template

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,6 @@
 import { defineConfig } from "vite";
-import melangePlugin from "vite-plugin-melange";
 import nodeResolve from "@rollup/plugin-node-resolve";
 
 export default defineConfig({
-	plugins: [
-		nodeResolve(),
-		melangePlugin({
-      emitDir: "src",
-      buildTarget: "output",
-      buildCommand: "exit 0",
-			watchCommand: "esy dune build @react --watch"
-		})
-	]
+	plugins: [nodeResolve()]
 })
